c72088b7b4b49d648e76cce1ae96928764d6bf62
JohnZBurnett <tuninator@gmail.com>
2018-07-05 14:25:49 -0400

    retrieve pokemon from db

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -6,23 +6,28 @@ const mainTag = document.querySelector("main");
 document.addEventListener("DOMContentLoaded", function(event) {
 
 
-  function loadAndDisplayData() {
-    fetch(TRAINERS_URL).then( function(response) { return response.json() }).then( function(trainersJson) { handleDisplayingTrainers(trainersJson)})
+
+  function createAndAppendPokemon(trainerObj, newTrainerElement) {
+    const pokemonArr = trainerObj.pokemons;
+    pokemonArr.forEach( function(pokemonObj) {
+      newTrainerElement.append(createPokemonElement(pokemonObj))
+    })
+    debugger;
   }
 
-  function handleDisplayingTrainers(trainersJson) {
-    trainersJson.forEach( function(trainer) { displayTrainer(trainer)});
-  }
+  function createPokemonElement(pokemonObj) {
 
+  }
+  function createTrainerElement(trainerObj) {
+    const newTrainerElement = document.createElement("div");
+    formatTrainerElementInformation(trainerObj, newTrainerElement);
+    createAndAppendPokemon(trainerObj, newTrainerElement);
+    mainTag.append(newTrainerElement);
+    debugger;
+  }
 
   function displayTrainer(trainerObj) {
     let trainerElement = createTrainerElement(trainerObj);
-  }
-
-  function createTrainerElement(trainerObj) {
-    const newTrainerElement = document.createElement("div");
-    mainTag.append(formatTrainerElementInformation(trainerObj, newTrainerElement));
-    debugger;
   }
 
   function formatTrainerElementInformation(trainerObj, newTrainerElement) {
@@ -31,6 +36,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
     newTrainerElement.innerHTML += `<p>${trainerObj.name}</p>`
     newTrainerElement.innerHTML += `<button data-trainer-id="${trainerObj.id}">Add Pokemon</button>`
     return newTrainerElement;
+  }
+
+  function handleDisplayingTrainers(trainersJson) {
+    trainersJson.forEach( function(trainer) { displayTrainer(trainer)});
+  }
+
+  function loadAndDisplayData() {
+    fetch(TRAINERS_URL).then( function(response) { return response.json() }).then( function(trainersJson) { handleDisplayingTrainers(trainersJson)})
   }
   loadAndDisplayData();
 
