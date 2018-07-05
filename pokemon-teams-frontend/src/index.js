4333adb368e9401b2a89552c4e20a9789e9d580a
JohnZBurnett <tuninator@gmail.com>
2018-07-05 14:04:47 -0400

    enable DOMContentLoaded

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,13 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+
+  function loadAndDisplayData() {
+    fetch(TRAINERS_URL).then( function(response) { return response.json() }).then( function(trainerJson) { console.log(trainerJson)})
+  }
+
+  loadAndDisplayData();
+})
